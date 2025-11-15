ef3e227958890225246b81be90ab948597c645fb
Hokoala <jean-michel.le@etudiant.univ-reims.fr>
2025-11-15 09:51:39 +0100

    feat : add Tailwing + API Spotify

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,22 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
     settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', '@typescript-eslint'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './APP.jsx'
-import './index.css'
+import App from './App.tsx'
+import '../public/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 )
+
